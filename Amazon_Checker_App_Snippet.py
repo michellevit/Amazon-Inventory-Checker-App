413804b630d034a377a904b456a4d40ec88658cf
michellevit <mvflandin@gmail.com>
2025-11-15 12:11:15 -0800

    Add demo gif to README

---
+++ Amazon_Checker_App_Snippet.py
@@ -0,0 +1,303 @@
+# Amazon-Checker-GUI/scripts/step_2_scripts.py
+# GitHub Repository: https://github.com/michellevit/Amazon-Inventory-Checker-App
+#
+# This script is part of my Amazon Inventory Checker App project.
+# I’m especially proud of this application because it was the first project I ever built. 
+# Over the years, as I’ve learned and grown, I’ve refactored it countless times - improving it piece by piece, and so it has become a sort of reflection of my entire learning journey.
+#
+# I chose this snippet because it tackles a really interesting real-world optimization problem.
+# Before I had even learned what data structure and algorithms were, this project forced me to consider how many different ways this optimization problem could be tackled, and how minor changes in the order would create major differences in the efficiency and end result.
+#
+# For a demo which explains the algorithm challenges with a clear example, see my demo video at timestamp 3:22: https://www.youtube.com/watch?v=lD-wTry930w&t=25s
+#
+# While I'm very proud of it, now that I've learned more about object oriented structures, I’d like to refactor it into a cleaner, class-based design as my next step.
+# Thanks for taking the time to check it out!
+
+
+import os
+from openpyxl import load_workbook
+from openpyxl.workbook import Workbook
+from openpyxl.utils.datetime import from_excel
+import xlwt
+import sys
+
+def calculate_units_to_cancel(requested_inventory, available_inventory):
+    units_to_cancel = {}
+    for model_number in requested_inventory:
+        if requested_inventory[model_number] > available_inventory[model_number]:
+            discrepancy = requested_inventory[model_number] - available_inventory[model_number]
+            units_to_cancel[model_number] = discrepancy    
+    return units_to_cancel
+
+
+def cancel_out_of_stock_units(units_to_cancel, available_inventory, processing_filenames, processing_dir, min_order_value_us, min_order_value_ca):
+    new_workbook = Workbook()
+    new_sheet = new_workbook.active
+    new_sheet.title = 'Combined Data'
+    order_value_dict = {}
+    current_row = 1
+    
+    # Create new temporary sheet with CA + USD data
+    for filename in processing_filenames.values():
+        processed_file_path = os.path.join(processing_dir, filename)
+        processing_workbook = load_workbook(processed_file_path)
+        reference_sheet = processing_workbook.active
+        current_row = copy_data_to_new_sheet(reference_sheet, new_sheet, current_row)
+    
+    # Get order values
+    order_value_dict = create_order_value_dict(new_sheet, order_value_dict)
+    # First Pass: remove OOS units without putting orders below threshold (+ update order_value_dict)
+    units_to_cancel, order_value_dict = remove_oos_units_above_threshold(new_sheet, units_to_cancel, order_value_dict, min_order_value_us, min_order_value_ca)
+    # If more units need to be cancelled
+    
+    if units_to_cancel: 
+        # sort order value dict low to high
+        sorted_order_value_list = sorted(order_value_dict.items(), key=lambda item: item[1])
+        # sort new sheet low to high
+        sort_sheet_by_order_value(new_sheet, sorted_order_value_list, new_workbook)
+        # Second Pass: iterate through temp sheet and remove remianing OOS units
+        remove_oos_units_below_threshold(new_sheet, units_to_cancel)
+        # Get updated order values
+        order_value_dict = create_order_value_dict(new_sheet, order_value_dict)
+        # Cancel orders below the minimum threshold
+        cancel_orders_below_min_again(new_sheet, min_order_value_us, min_order_value_ca, order_value_dict)
+
+    # Update the global inventory_dict (for the copy function)
+    confirmed_inventory_dict = get_confirmed_inventory(new_sheet)
+
+    # Check if there is any unallocated in-stock units (from orders which were cancelled in the second pass)
+    extra_units_dict = get_extra_units(available_inventory, confirmed_inventory_dict)
+
+    if extra_units_dict:
+        # Get updated order values
+        order_value_dict = create_order_value_dict(new_sheet, order_value_dict)
+        # Add unallocated units to accepted orders
+        allocate_extra_units_to_orders(new_sheet, extra_units_dict, order_value_dict)
+        # Get the updated accepted_inventory_dict
+        confirmed_inventory_dict = get_confirmed_inventory(new_sheet)
+
+    # Create a new dict with order data from the new_sheet (order number, model number, units confirmed)
+    final_order_data_dict = create_final_order_data_dict(new_sheet)
+
+    # Update original sheet(s) with updated quantities + availability status
+    update_original_sheets(processing_filenames, processing_dir, final_order_data_dict)
+
+    # Save the new_sheet for debugging       
+    new_file_path = os.path.join(processing_dir, 'Combined-Orders-Processing.xlsx')
+    new_workbook.save(new_file_path)
+    return confirmed_inventory_dict
+
+
+def copy_data_to_new_sheet(reference_sheet, new_sheet, start_row):
+    for row in range(4, reference_sheet.max_row + 1):
+        if not reference_sheet[f'A{row}'].value:
+            break
+        for col in range(1, reference_sheet.max_column + 1):
+            new_sheet.cell(row=start_row, column=col, value=reference_sheet.cell(row=row, column=col).value)
+        start_row += 1
+    return start_row
+
+
+def create_order_value_dict(sheet, order_value_dict):
+    order_value_dict = {}
+    for row in range(1, sheet.max_row + 1):
+        order_number = sheet[f'A{row}'].value
+        list_price = sheet[f'I{row}'].value
+        confirmed_qty = sheet[f'L{row}'].value
+        cost = list_price * confirmed_qty
+        if order_number in order_value_dict.keys():
+            order_value_dict[order_number] = order_value_dict[order_number] + cost
+        else:
+            order_value_dict[order_number] = cost
+    return order_value_dict
+
+
+def remove_oos_units_above_threshold(sheet, units_to_cancel, order_value_dict, min_order_value_us, min_order_value_ca):
+    min_order_value_us = float(min_order_value_us.get())
+    min_order_value_ca = float(min_order_value_ca.get())
+    for row in range(1, sheet.max_row + 1):
+        if not units_to_cancel:
+            break
+        model_number = sheet[f'C{row}'].value
+        if model_number in units_to_cancel.keys():
+            cancel_amount = units_to_cancel[model_number]
+            order_number = sheet[f'A{row}'].value
+            price = sheet[f'I{row}'].value
+            confirmed_qty = sheet[f'L{row}'].value
+            currency = sheet[f'AF{row}'].value
+            if currency == "USD":
+                min_val = min_order_value_us
+            else:
+                min_val = min_order_value_ca
+            while confirmed_qty > 0 and cancel_amount > 0:
+                if (order_value_dict[order_number] - price) > min_val:
+                    # confirmed_quantity: remove 1
+                    sheet[f'L{row}'].value = sheet[f'L{row}'].value - 1
+                    # update units_to_cancel
+                    units_to_cancel[model_number] = units_to_cancel[model_number] - 1
+                    # update order_value_dict
+                    order_value_dict[order_number] = order_value_dict[order_number] - price
+                    # update counters
+                    confirmed_qty = confirmed_qty - 1
+                    cancel_amount = cancel_amount - 1
+                else:
+                    break
+            if units_to_cancel[model_number] == 0:
+                del units_to_cancel[model_number]
+    return units_to_cancel, order_value_dict
+            
+
+def sort_sheet_by_order_value(sheet, sorted_order_value_list, new_workbook):
+    temp_sheet = new_workbook.create_sheet("TempSorted")
+    new_row_idx = 1
+    for order_number, _ in sorted_order_value_list:
+        for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, values_only=False):
+            if row[0].value == order_number:  # Assuming order_number is in the first column (column A)
+                for col_idx, cell in enumerate(row, start=1):
+                    temp_sheet.cell(row=new_row_idx, column=col_idx, value=cell.value)
+                new_row_idx += 1
+    for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row):
+        for cell in row:
+            cell.value = None
+    for row_idx, row in enumerate(temp_sheet.iter_rows(min_row=1, max_row=temp_sheet.max_row, values_only=False), start=1):
+        for col_idx, cell in enumerate(row, start=1):
+            sheet.cell(row=row_idx, column=col_idx, value=cell.value)
+    new_workbook.remove(temp_sheet)
+
+
+def remove_oos_units_below_threshold(sheet, units_to_cancel):
+    for row in range(1, sheet.max_row + 1):
+        order_number = sheet[f'A{row}'].value
+        if not units_to_cancel:
+            break
+        model_number = sheet[f'C{row}'].value
+        if model_number in units_to_cancel.keys():
+            cancel_amount = units_to_cancel[model_number]
+            confirmed_qty = sheet[f'L{row}'].value
+            while confirmed_qty > 0 and cancel_amount > 0:
+                # confirmed_quantity: remove 1
+                sheet[f'L{row}'].value = sheet[f'L{row}'].value - 1
+                # update units_to_cancel
+                units_to_cancel[model_number] = units_to_cancel[model_number] - 1
+                # update counters
+                confirmed_qty = confirmed_qty - 1
+                cancel_amount = cancel_amount - 1
+            if units_to_cancel[model_number] == 0:
+                del units_to_cancel[model_number]
+
+
+def cancel_orders_below_min_again(sheet, min_order_value_us, min_order_value_ca, order_value_dict):
+    for row in range(1, sheet.max_row + 1):
+        if not sheet[f'A{row}'].value:
+            break
+        order_number = sheet[f'A{row}'].value
+        model_number = sheet[f'C{row}'].value
+        currency = sheet[f'AF{row}'].value
+        if currency == "USD":
+            minimum = int(min_order_value_us.get())
+        else: 
+            minimum = int(min_order_value_ca.get())
+        if order_value_dict[order_number] < minimum:
+            sheet[f'L{row}'].value = 0.0
+            sheet[f'S{row}'].value = "CA - Cancelled: Not yet available"        
+
+
+def get_confirmed_inventory(sheet):
+    inventory_dict = {}
+    for row in range(1, sheet.max_row + 1):
+        if not sheet[f'A{row}'].value:
+            break    
+        model_number = sheet[f'C{row}'].value
+        confirmed_qty = sheet[f'L{row}'].value
+        if confirmed_qty != 0:
+            if model_number in inventory_dict.keys():
+                inventory_dict[model_number] = inventory_dict[model_number] + confirmed_qty
+            else: 
+                inventory_dict[model_number] = confirmed_qty
+    return inventory_dict
+
+
+def get_extra_units(available_inventory, confirmed_inventory_dict):
+    extra_units_dict = {}
+    for model in available_inventory.keys():
+        if model in confirmed_inventory_dict.keys():
+            variance = available_inventory[model] - confirmed_inventory_dict[model]
+            if variance > 0:
+                extra_units_dict[model] = variance
+        else:
+            extra_units_dict[model] = available_inventory[model]
+    return extra_units_dict
+
+
+def allocate_extra_units_to_orders(new_sheet, extra_units_dict, order_value_dict):
+    for row in range(1, new_sheet.max_row + 1):
+        order_number = new_sheet[f'A{row}'].value
+        model_number = new_sheet[f'C{row}'].value
+        requested_qty = new_sheet[f'J{row}'].value
+        confirmed_qty = new_sheet[f'L{row}'].value
+        if confirmed_qty < requested_qty:
+            if model_number in extra_units_dict and order_value_dict.get(order_number, 0) > 0:
+                while confirmed_qty < requested_qty and extra_units_dict[model_number] > 0:
+                    new_sheet[f'L{row}'].value += 1
+                    extra_units_dict[model_number] -= 1
+                if extra_units_dict[model_number] == 0:
+                    del extra_units_dict[model_number]
+          
+
+def create_final_order_data_dict(sheet):
+    order_dict = {}
+    for row in range(1, sheet.max_row + 1):
+        order_number = sheet[f'A{row}'].value
+        model_number = sheet[f'C{row}'].value
+        confirmed_qty = sheet[f'L{row}'].value
+        if order_number not in order_dict:
+            order_dict[order_number] = {}
+        order_dict[order_number][model_number] = confirmed_qty
+    return order_dict
+
+
+def update_original_sheets(processing_filenames, processing_dir, order_dict):
+    for filename in processing_filenames.values():
+        processed_file_path = os.path.join(processing_dir, filename)
+        processing_workbook = load_workbook(processed_file_path)
+        processing_sheet = processing_workbook.active
+        for row in range(4, processing_sheet.max_row + 1):
+            order_number = processing_sheet[f'A{row}'].value
+            model_number = processing_sheet[f'C{row}'].value
+            if order_number in order_dict and model_number in order_dict[order_number]:
+                new_confirmed_qty = order_dict[order_number][model_number]
+                processing_sheet[f'L{row}'].value = new_confirmed_qty
+                if new_confirmed_qty == 0:
+                    processing_sheet[f'S{row}'].value = "CA - Cancelled: Not yet available"
+        processing_workbook.save(processed_file_path)
+
+
+def convert_xlsx_to_xls(filename, processing_dir, upload_directory):
+    processed_file_path = os.path.join(processing_dir, filename)
+    processing_workbook = load_workbook(processed_file_path)
+    reference_sheet = processing_workbook.active
+    processing_workbook.close()
+    completed_workbook = xlwt.Workbook()
+    new_sheet = completed_workbook.add_sheet('Line Items')
+
+    for row_idx, row in enumerate(reference_sheet.iter_rows()):
+        for col_idx, cell in enumerate(row):
+            if cell.is_date:
+                date_value = cell.value
+                date_string = date_value.strftime("%d-%b-%Y")
+                new_sheet.write(row_idx, col_idx, date_string)
+            else:
+                new_sheet.write(row_idx, col_idx, cell.value)
+    
+    # Require sheet - no need for contents, but it must be added or else Amazon will reject the file
+    completed_workbook.add_sheet('Instructions')
+    
+    completed_filename = filename.replace('Processing.xlsx', 'Complete.xls')
+    completed_file_path = os.path.join(upload_directory, completed_filename)
+    completed_workbook.save(completed_file_path)
+    
+    print("processing_workbook: ", processing_workbook)
+    print("processed_file_path: ", processed_file_path)
+    return completed_workbook
+
+
--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 # Amazon Inventory Checker App
 
-
 ![Python Version](https://img.shields.io/badge/Python-3.10.4-ffdb4f.svg)
 ![Tkinter](https://img.shields.io/badge/Tkinter-8.6-3A77A8.svg)
 ![openpyxl](https://img.shields.io/badge/OpenPyXL-3.1.2-206e47.svg)
 ![PyInstaller](https://img.shields.io/badge/PyInstaller-6.6.0-8CA1AF.svg)
 
-
 An application that intakes an Amazon order request spreadsheet, calculates the total requested inventory, allows users to input their actual inventory, and calculates the optimal orders to confirm based on inventory and the minimum order value threshold (to keep shipping costs reasonable).
-
 
 <a href="https://youtu.be/WcHKO0UPXGo?si=MWx0eaWaXHBHora6" target="_blank"><img src="https://img.shields.io/badge/YouTube-Demo-red?style=for-the-badge&logo=youtube&color=FF0000"></a>
 
+![Amazon Inventory Checker Demo](resources/amazon-checker-demo-gif.gif)
 
 ## Table of Contents
+
 - [Technologies Used](#technologies-used)
 - [How To Install the Program](#how-to-install-the-program)
 - [How To Edit the Program](#how-to-edit-the-program)
@@ -21,49 +20,50 @@ An application that intakes an Amazon order request spreadsheet, calculates the 
 - [To Do](#to-do)
 - [Credits](#credits)
 
-
 ## Technologies Used<a name="technologies-used"></a>
+
 - Python (3.10.0)
 - Tkinter (8.6)
 - OpenPyXL Library (3.1.2)
 - PyInstaller (6.6.0)
 
-
 ## How To Install the Program
-* Note: these instructions are intended for users who are using the app, but not editing the app
-  * Editing the app requires cloning a local copy of repo, installing Python, and creating/activating a virtual environment
-- Open the [project's GitHub repository](https://github.com/michellevit/Amazon-Inventory-Checker-App)
-- Select the green 'Code' button, and select 'Download ZIP'
-- Find the newly downloaded zip folder (in the Download tab, select 'Show in folder')
-- Right click the .zip folder, click 'Extract All...', and select the desired destination folder for the application
-   - Note: make sure to save the folder directly onto your personal computer, not on a remote server (due to necessary file permissions).
-- Open the unzipped folder and double-click the 'app-installer.bat' file
+
+- Note: these instructions are intended for users who are using the app, but not editing the app
+  - Editing the app requires cloning a local copy of repo, installing Python, and creating/activating a virtual environment
+
+* Open the [project's GitHub repository](https://github.com/michellevit/Amazon-Inventory-Checker-App)
+* Select the green 'Code' button, and select 'Download ZIP'
+* Find the newly downloaded zip folder (in the Download tab, select 'Show in folder')
+* Right click the .zip folder, click 'Extract All...', and select the desired destination folder for the application
+  - Note: make sure to save the folder directly onto your personal computer, not on a remote server (due to necessary file permissions).
+* Open the unzipped folder and double-click the 'app-installer.bat' file
   - A new shortcut will be created on the Desktop for the application
   - Note: if you move the location of the application folder, you will need to delete the shortcut and run 'app-installer.bat' again
 
-
 ## How To Edit the Program<a name="how-to-edit"></a>
+
 - Open the app-builder.bat and change DEBUG value to TRUE (to see print statements)
 - Activate the Virtual Terminal - run: `.\venv\Scripts\activate`
 - After making code changes to the code - run: `.\app-builder.bat`
 - Run the app from the command line to see print statements in the terminal: `.\AmazonChecker.exe`
 - Troubleshooting Notes:
   - Note 1: For unsuccessful build issues, see [Troubleshooting section](#troubleshooting)
-  - Note 2: The virtual environment must be created and activated to run the build script (but not 
+  - Note 2: The virtual environment must be created and activated to run the build script (but not
     needed for running the program itself due to pyinstaller):
-      - To create the virtual environment:
-        - Ensure Python3 is installed on your system
-        - Open a terminal
-        - Navigate to project's root directory
-        - Run: `python -m venv venv`
-        - Run: `pip install -r requirements.txt`
-      - To activate the virtual environment:
-        - Open a terminal and navigate to the project's root dir 
-        - Run: `.\venv\Scripts\activate`
+    - To create the virtual environment:
+      - Ensure Python3 is installed on your system
+      - Open a terminal
+      - Navigate to project's root directory
+      - Run: `python -m venv venv`
+      - Run: `pip install -r requirements.txt`
+    - To activate the virtual environment:
+      - Open a terminal and navigate to the project's root dir
+      - Run: `.\venv\Scripts\activate`
   - Note 3: After the program is done being edited, open the app-builder.bat and make sure the DEBUG value is set to FALSE and run the app-builder again
 
-
 ## Troubleshooting<a name="troubleshooting"></a>
+
 - If `.\app-builder` does not work:
   - Make sure the app is closed and the spreadsheets being used are closed
 - If app-builder.bat encounters the error `Operation did not complete successfully because the file contains a virus or potentially unwanted software.`:
@@ -78,6 +78,6 @@ An application that intakes an Amazon order request spreadsheet, calculates the 
 - If the terminal opens after starting the program:
   - Make sure the DEBUG value is set to FALSE in app-builder.bat
 
-
 ## Credits<a name="credits"></a>
+
 Michelle Flandin
